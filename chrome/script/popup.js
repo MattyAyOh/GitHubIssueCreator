136a66c34c5706fb1bb23a459b37054b6d549e63
Matt Ao <m.ao@techsmith.com>
2015-12-02 17:52:24 -0500

    Always create a new tab

--- chrome/script/popup.js
+++ chrome/script/popup.js
@@ -29,12 +29,9 @@ $(function() {
     chrome.runtime.sendMessage({method: "saveTemplateLocation", location: 'https://rawgit.com/' + user + '/' + repo + '/master/ISSUETEMPLATE.md'}, function(response) {});
     // Attempt to fetch the instance of the currently-open tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      // Redirect the current tab if one is found, otherwise create a new one
-      if (tabs.length) {
-        chrome.tabs.update(tabs[0].id, {url: repoLocation});
-      } else {
-        chrome.tabs.create({url: repoLocation});
-      }
+
+    chrome.tabs.create({url: repoLocation});
+      
     });
     window.close();
   });
