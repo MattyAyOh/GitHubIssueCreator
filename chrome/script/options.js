7a00dcf8e5908188538ffd596bbaa41e8e7a93d4
Matt Ao <m.ao@techsmith.com>
2015-12-02 16:33:10 -0500

    Initial Commit, custom template

---
+++ chrome/script/options.js
@@ -0,0 +1,16 @@
+var STORAGE_KEY = 'github-repos';
+
+$(function() {
+  var $textarea = $('#repos');
+  var $submitButton = $('#save');
+
+  if (localStorage[STORAGE_KEY] === undefined) {
+    localStorage[STORAGE_KEY] = 'MattyAyOh/GitHubIssueGenerator';
+  }
+
+  $textarea.val(localStorage[STORAGE_KEY]);
+
+  $submitButton.click(function() {
+    localStorage[STORAGE_KEY] = $textarea.val();
+  });
+});
---
+++ template.md
@@ -0,0 +1,23 @@
+##### Specific Details
+
+##### Steps to Reproduce
+
+1. 
+
+2. 
+
+3. 
+
+##### Expected Results
+
+##### Actual Results
+
+---
+
+###### Testing Notes
+
+###### Developer Notes
+
+###### Build
+
+###### Mac OS
