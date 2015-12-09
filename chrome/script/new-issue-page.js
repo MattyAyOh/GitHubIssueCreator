59c249ed3410649eeb9a556fa5be13eaa627cd83
Matt Ao <m.ao@techsmith.com>
2015-12-09 11:04:13 -0500

    Proper error handling in the event that a repository does not have a template

--- chrome/script/new-issue-page.js
+++ chrome/script/new-issue-page.js
@@ -2,7 +2,7 @@ var $ISSUE_TITLE = $('.composer [name="issue[title]"]');
 var $ISSUE_BODY = $('.composer [name="issue[body]"]');
 
 $(function() {
-  var TEMPLATE_PATH = 'https://rawgit.com/MattyAyOh/GitHubIssueGenerator/master/README.md';
+  var TEMPLATE_PATH = 'https://github.com/MattyAyOh/GitHubIssueGenerator/master/ISSUETEMPLATE.md';
 
   chrome.runtime.sendMessage(
     {method: "getTemplateLocation"}, function(response) 
@@ -35,6 +35,40 @@ $(function() {
         {
           $ISSUE_BODY.prop('placeholder', "Couldn't fetch issue template. Sorry!");
         }
+      }).fail(function() 
+      {
+        $ISSUE_TITLE.prop('placeholder', "Title (ERROR: ISSUETEMPLATE.MD NOT FOUND.  LOADING DEFAULT TEMPLATE)");
+
+        var TEMPLATE_PATH = 'https://github.com/MattyAyOh/GitHubIssueGenerator/blob/master/ISSUETEMPLATE.md';
+        $.get(TEMPLATE_PATH, function(contents, status) 
+        {
+          console.log(status);
+          if (status == 'success') 
+          {
+            var markdown = "";
+            if( contents.indexOf("<code>") > -1 )
+            {
+              markdown = contents.split("<code>")[1].split("</code>")[0];
+            }
+
+            if( markdown == "" )
+            {
+              $ISSUE_BODY.prop('placeholder', "ERROR: ISSUETEMPLATE.md is not properly indented (markdown requires indents [4 spaces] to denote code)");
+            }
+            else
+            {
+              $ISSUE_BODY.prop('placeholder', "HEY!  Don't ignore the issue template.");            
+            }
+            $ISSUE_BODY.val(markdown);
+          } 
+          else 
+          {
+            $ISSUE_BODY.prop('placeholder', "Couldn't fetch issue template. Sorry!");
+          }
+        }).fail(function() 
+        {
+          $ISSUE_BODY.prop('placeholder', "Couldn't fetch issue template. Sorry!");
+        });
       });
 
 
